8d1d630be56e8978a793c00f770892564a677ecd
Durgesh-Bluesparing <durgesh@bluesparing.com>
2024-11-14 12:16:50 +0530

    update footer font size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
   return (
     <div className="py-2 fixed w-[100%]  bottom-0 bg-white z-[200]">
-      <p className="text-lg font-semibold text-center">
+      <p className="text-sm font-semibold text-center">
         © {currentYear} Blue Sparing. All rights reserved. |
         <span className="text-xs text-gray-500 mt-0">
           Designed with ❤️ by BLUE SPARING
