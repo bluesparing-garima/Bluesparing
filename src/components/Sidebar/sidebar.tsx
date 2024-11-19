8191fa8f79b8d60d945ca89658efe8788ac690fb
bluesparing-garima <garima@bluesparing.com>
2024-11-19 12:27:04 +0530

    change logo code

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import logo from "../../assets/logo.svg";
-import useLogoClickHandler from "../../utils/useLogoClickHandler";
+import logo from "../../assets/login_logo.png";
 import { IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
+import { imagePath } from "../../context/constant";
 type MenuItem = {
   id: number;
   label: string;
@@ -9,7 +10,6 @@ type MenuItem = {
   link?: string;
   subMenu?: SubMenuItem[];
 };
-
 type SubMenuItem = {
   id: number;
   svgIcon?: string;
@@ -21,7 +21,6 @@ interface SidebarProps {
   setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
-  // Sample menu items data
   const menuItems: MenuItem[] = [
     {
       id: 1,
@@ -34,14 +33,14 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
       label: "Admin",
       svgIcon: "M12 6v12m6-6H6",
       subMenu: [
+        // {
+        //   id: 20,
+        //   label: "Role",
+        //   link: "/#/role",
+        //   svgIcon: "M4 6h16M4 12h16M4 18h16",
+        // },
         {
           id: 20,
-          label: "Role",
-          link: "/#/role",
-          svgIcon: "M4 6h16M4 12h16M4 18h16",
-        },
-        {
-          id: 214,
           label: "Rank",
           link: "/#/ranks",
           svgIcon:
@@ -54,7 +53,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
           svgIcon:
             "M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z",
         },
-
         {
           id: 22,
           label: "Broker",
@@ -190,7 +188,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
           svgIcon:
             "M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5",
         },
-
         {
           id: 42,
           label: "Pay  Out",
@@ -257,7 +254,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
             "M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25m-2.25 0h-2.25m0 0v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v4.964m12-4.006v4.006m0 0v3.75m-12-7.756v3.75m0 0h12m-12 0V14.25m12-4.5V9m0 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25m-2.25 0h-2.25m0 0v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v4.964m12-4.006v4.006m0 0v3.75m-12-7.756v3.75m0 0h12m-12 0V14.25m12-4.5V9",
         },
         {
-          id: 61,
+          id: 62,
           label: "Accepted",
           link: "/#/booking/accepted",
           svgIcon:
@@ -265,7 +262,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
         },
       ],
     },
-
     {
       id: 8,
       label: "Account",
@@ -302,7 +298,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
         },
       ],
     },
-
     {
       id: 10,
       label: "Compare Excel",
@@ -333,17 +328,14 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
         "M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z",
     },
   ];
-
-  // State to manage active menu item and open/close state of submenus
   const [activeMenuItem, setActiveMenuItem] = useState<number | null>(null);
   const [openSubMenus, setOpenSubMenus] = useState<number[]>([]);
-
-  // Function to handle menu item click
   const handleMenuItemClick = (itemId: number) => {
     setActiveMenuItem(itemId === activeMenuItem ? null : itemId);
   };
 
-  // Function to toggle submenu open/close
+  const storedTheme: any = localStorage.getItem("user");
+  const UserData = storedTheme ? JSON.parse(storedTheme) : null;
   const toggleSubMenu = (parentId: number) => {
     if (openSubMenus.includes(parentId)) {
       setOpenSubMenus(openSubMenus.filter((id) => id !== parentId));
@@ -352,22 +344,13 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
     }
   };
 
-  const handleLogoClick = useLogoClickHandler();
   return (
     <div
-      className={`${
+      className={` ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
       } md:translate-x-0 sticky  top-0  z-20 md:flex flex-col w-60 bg-white h-screen shadow-lg border-r-2 border-[#FEF9F3] transition-transform delay-150 duration-200`}
     >
-      <div className="flex items-center justify-center h-16 bg-white">
-        <img
-          src={logo}
-          onClick={handleLogoClick}
-          className="w-32 cursor-pointer"
-          alt="Logo"
-        />
-      </div>
-      <div className="md:hidden flex w-full justify-end">
+      <div className="md:hidden flex w-full justify-end ">
         <IconButton onClick={() => setSidebarOpen((prev) => !prev)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -386,14 +369,36 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
         </IconButton>
       </div>
       <div className="flex flex-col flex-1 overflow-hidden">
-        <div className="flex-1 py-7 overflow-y-auto bg-white">
+        <Link to="/dashboard" className="mx-1 my-2">
+          <picture className="mb-1 flex flex-col justify-center items-center ">
+            {UserData.companyLogo ? (
+              <>
+                <source
+                  srcSet={`${imagePath}/${UserData.companyLogo}`}
+                  type="image/png"
+                />
+                <img
+                  src={`${imagePath}/${UserData.companyLogo}`}
+                  className="w-36 h-12 mx-auto"
+                  alt="company Logo"
+                />
+              </>
+            ) : (
+              <>
+                <source srcSet={logo} type="image/png" />
+                <img src={logo} className="w-44 mx-auto" alt="company Logo" />
+              </>
+            )}
+          </picture>
+        </Link>
+        <div className="flex-1 py-7 overflow-y-auto bg-white hide-scrollbar">
           <ul className="space-y-2">
             {menuItems.map((item) => (
               <li
                 key={item.id}
                 className={
                   activeMenuItem === item.id
-                    ? "bg-safekaroDarkBlue text-white"
+                    ? "bg-safekaroDarkOrange text-white"
                     : "text-white-500"
                 }
               >
@@ -402,8 +407,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
                     <div
                       className={
                         activeMenuItem === item.id
-                          ? "flex text-sm items-center text-white px-2 py-1 cursor-pointer hover:bg-safekaroDarkBlue hover:text-white rounded-lg"
-                          : "flex text-sm items-center px-2 py-1 cursor-pointer hover:bg-safekaroDarkBlue hover:text-white rounded-lg"
+                          ? "flex text-sm items-center text-white px-2 py-1 cursor-pointer hover:bg-safekaroDarkOrange hover:text-white rounded-lg"
+                          : "flex text-sm items-center px-2 py-1 cursor-pointer hover:bg-safekaroDarkOrange hover:text-white rounded-lg"
                       }
                       onClick={() => toggleSubMenu(item.id)}
                     >
@@ -436,7 +441,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
                               <a
                                 href={subItem.link}
                                 onClick={() => handleMenuItemClick(subItem.id)}
-                                className="flex text-sm items-center px-2 py-1 text-gray-500 hover:bg-safekaroDarkBlue hover:text-white rounded-lg"
+                                className="flex text-sm items-center px-2 py-1 text-gray-500 hover:bg-safekaroOrange hover:text-white rounded-lg"
                               >
                                 <svg
                                   xmlns="http://www.w3.org/2000/svg"
@@ -465,8 +470,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
                     onClick={() => handleMenuItemClick(item.id)}
                     className={
                       activeMenuItem === item.id
-                        ? "flex text-sm items-center px-2 py-1 ml-3 rounded-lg text-white hover:bg-safekaroDarkBlue hover:text-white"
-                        : "flex text-sm items-center px-2 py-1 ml-3 text-black hover:bg-safekaroDarkBlue hover:text-white rounded-lg"
+                        ? "flex text-sm items-center px-2 py-1 ml-3 rounded-lg text-white hover:bg-safekaroDarkOrange hover:text-white"
+                        : "flex text-sm items-center px-2 py-1 ml-3 text-black hover:bg-safekaroDarkOrange hover:text-white rounded-lg"
                     }
                   >
                     <svg
@@ -492,8 +497,16 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
           </ul>
         </div>
       </div>
+      <div className="mx-1 flex flex-col justify-center items-center">
+        <span className="text-sm font-medium text-safekarolightOrange">
+          Powered By
+        </span>
+        <picture className="mb-1 ">
+          <source srcSet={logo} type="image/png" />
+          <img src={logo} className="w-44 mx-auto" alt="company Logo" />
+        </picture>
+      </div>
     </div>
   );
 };
-
 export default Sidebar;
