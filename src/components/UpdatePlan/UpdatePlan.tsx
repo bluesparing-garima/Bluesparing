edcf71b8c1b67a6854c2e1dabfa4199d0595d1e6
Durgesh-Bluesparing <durgesh@bluesparing.com>
2024-12-23 10:23:42 +0530

    add heading in plan

--- src/components/UpdatePlan/UpdatePlan.tsx
+++ src/components/UpdatePlan/UpdatePlan.tsx
@@ -7,7 +7,7 @@ const UpdatePlan = () => {
 
   return (
     <div className="bg-blue-200 lg:p-5  p-4">
-
+      <h1 className="w-full text-center mb-4 text-2xl uppercase font-extrabold underline  text-[#213555]">Choose your plan</h1>
       <Grid container spacing={2} gap={3} padding={2}>
         {subsData.map((ele, index) => (
           <PlanCard key={`${ele.planName}${index}`} p={ele} />
