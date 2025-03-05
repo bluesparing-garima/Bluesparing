16b15e5b12e51e459931e9685dcc69095cd82ab2
bluesparing-shubham <shubhamgym2000@gmail.com>
2025-03-05 16:30:56 +0530

    `Switched API endpoint host to localhost for development purposes`

--- src/api/Token/apiEndpoints.ts
+++ src/api/Token/apiEndpoints.ts
@@ -1,4 +1,4 @@
-const API_ENDPOINT_HOST = "https://iimapi.bluesparing.com"
-// const API_ENDPOINT_HOST = "http://localhost:7000";
+// const API_ENDPOINT_HOST = "https://iimapi.bluesparing.com"
+const API_ENDPOINT_HOST = "http://localhost:7000";
 export const refreshToken = () =>
     API_ENDPOINT_HOST.concat(`/api/user/refresh-token`);
