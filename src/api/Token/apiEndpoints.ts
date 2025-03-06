49123e42cb7684799a63250e8581b15b6197c737
bluesparing-shubham <shubhamgym2000@gmail.com>
2025-03-06 15:49:10 +0530

    api endpoints

--- src/api/Token/apiEndpoints.ts
+++ src/api/Token/apiEndpoints.ts
@@ -3,8 +3,8 @@ import jsPDF from "jspdf";
 import { holidayDeleteEndpoint } from "../HR/ApiEndpoints";
 
 
-const API_ENDPOINT_HOST = "https://iimapi.bluesparing.com"
-// const API_ENDPOINT_HOST = "http://localhost:7000";
+// const API_ENDPOINT_HOST = "https://iimapi.bluesparing.com"
+const API_ENDPOINT_HOST = "http://localhost:7000";
 export const refreshToken = () =>
     API_ENDPOINT_HOST.concat(`/api/user/refresh-token`);
 
