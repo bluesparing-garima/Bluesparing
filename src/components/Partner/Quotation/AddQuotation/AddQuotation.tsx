dd1c24cd8cbbe939ae85707dcaf5249924c63964
bluesparing-abhishek6848 <bluesparingabhishek@gmail.com>
2025-03-18 17:29:05 +0530

    fixed issue of add quotation from frontend

--- src/components/Partner/Quotation/AddQuotation/AddQuotation.tsx
+++ src/components/Partner/Quotation/AddQuotation/AddQuotation.tsx
@@ -9,6 +9,8 @@ import {
   FormControl,
   Autocomplete,
   Button,
+  Tooltip,
+  IconButton,
 } from "@mui/material";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { Field, Form } from "react-final-form";
@@ -21,6 +23,7 @@ import {
   imagePath,
   policyStatusPartner,
   policyStatusOperation,
+  ALLOWED_FILE_TYPES,
 } from "../../../../context/constant";
 import { ILeadForm, IQuotations } from "../../IPartner";
 import { convertILeadVMToILeadForm } from "../../../../api/Leads/convertILeadVMToILeadForm";
@@ -32,6 +35,7 @@ import getQuotationByLeadIdService from "../../../../api/Quatotion/GetQuotationB
 import dayjs from "dayjs";
 import editLeadService from "../../../../api/Leads/EditLead/editLeadService";
 import toast, { Toaster } from "react-hot-toast";
+import { documentTypes } from "../../../Policy/IPolicyData";
 const AddQuotation = () => {
   const title = "Add Comment";
   let storedTheme: any = localStorage.getItem("user") as SafeKaroUser | null;
@@ -41,16 +45,20 @@ const AddQuotation = () => {
   const location = useLocation();
   const pathName = location.pathname.split("/");
   const isAdd = pathName[pathName.length - 1] === ADD;
+  const [errors, setErrors] = useState<{ docName: string; file: string }[]>([
+    { docName: "", file: "" },
+  ]);
   const [editLeadDetails, setEditLeadDetails] = useState<ILeadForm>();
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [image, setImage] = useState<any>(null);
   const [documents, setDocuments] = useState<Document[]>([
-    { docName: "", file: "" },
+    { docName: "currentPolicy", file: "" },
   ]);
   const [viewQuotationDetails, setViewQuotationDetails] = useState<
     IQuotations[]
   >([]);
+
   useEffect(() => {
     if (!isAdd && leadId) {
       getQuotationByLeadIdService({ header, leadId })
@@ -132,27 +140,86 @@ const AddQuotation = () => {
         });
     }
   }, [isAdd, leadId]);
-  const handleFileInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleFileInputChange = (event: any, index: any) => {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
+      const fileType = file.type;
       const fileSize = file.size;
-      if (fileSize > MAX_FILE_SIZE) {
-        setErrorMessage("Image is incorrect size.");
+      const newErrors = [...errors];
+      if (!ALLOWED_FILE_TYPES.includes(fileType)) {
+        newErrors[index] = {
+          ...newErrors[index],
+          file: "Invalid file type. Please upload an image or a PDF.",
+        };
+        setErrors(newErrors);
+      } else if (fileSize > MAX_FILE_SIZE) {
+        newErrors[index] = {
+          ...newErrors[index],
+          file: "File size exceeds the maximum limit",
+        };
+        setErrors(newErrors);
       } else {
         setErrorMessage("");
-        setImage(file);
+        const newDocuments = [...documents];
+        newDocuments[index] = { ...newDocuments[index], file: file };
+        setDocuments(newDocuments);
+        if (newErrors[index]) {
+          newErrors[index].file = "";
+        }
+        setErrors(newErrors);
       }
     }
   };
+
+  const validateDocument = (document: Document, index: number) => {
+    const isValidDocName = document.docName.trim() !== "";
+    const isValidFile = document.file;
+    validateField(index, "docName", document.docName);
+    validateField(index, "file", document.file);
+    return isValidDocName && isValidFile;
+  };
+
+  const validateField = (index: number, name: string, value: string) => {
+    const newErrors = [...errors];
+    if (name === "docName" || name === "file") {
+      newErrors[index] = {
+        ...newErrors[index],
+        [name]: value === "" ? `${name} cannot be empty` : "",
+      };
+    }
+    setErrors(newErrors);
+  };  
+
+  const handleChangeDocumentName = (newValue: any, index: any) => {
+    const updatedDocuments = documents.map((doc, i) =>
+      i === index ? { ...doc, docName: newValue?.value! } : doc
+    );
+    setDocuments(updatedDocuments);
+  };
+  
+  const handleClickAddDocument = () => {
+    setDocuments([...documents, { docName: "", file: "" }]);
+  };
+  const handleClickDeleteDocument = (index: any) => {
+    setDocuments((prevDocuments) =>
+      prevDocuments.filter((_, i) => i !== index)
+    );
+    const newErrors = errors.filter((_, i) => i !== index);
+    setErrors(newErrors);
+  };
+
   const onSubmit = (quotationForm: any, form: any) => {
+    console.log(quotationForm);
     quotationForm.relationshipManagerId = userData.headRMId;
     quotationForm.relationshipManagerName = userData.headRM;
     quotationForm.leadId = leadId;
     quotationForm.createdBy = userData.name;
     quotationForm.quotationImage = image;
     quotationForm.status = quotationForm.status.value;
+    documents?.forEach((ele)=>{
+      quotationForm[ele.docName] = ele.file
+    })
+    console.log('quotationForm',quotationForm)
     const editFrom = new FormData();
     editFrom.append("id", leadId!);
     editFrom.append("status", quotationForm.status);
@@ -185,33 +252,51 @@ const AddQuotation = () => {
         editLeadDetails?.timer
       );
     }
+    // console.log("Documents before append:", documents); // Debugging step
     callAddQuotationAPI(quotationForm, form);
   };
-  const callAddQuotationAPI = async (quotationForm: any, form: any) => {
+  
+  const callAddQuotationAPI = async (quotationForm: Record<string, any>, form: any) => {
     const formData = new FormData();
-    for (const [key, value] of Object.entries(quotationForm)) {
-      if (value instanceof File) {
-        formData.append(key, value);
-      } else {
-        formData.append(key, value as string);
-      }
-    }
+
+    // Append quotationForm fields
+    Object.entries(quotationForm).forEach(([key, value]) => {
+        if (value !== undefined) {
+            if (value instanceof File) {
+                formData.append(key, value);
+            } else {
+                formData.append(key, value as string);
+            }
+        }
+    });
+
+    // Append documents properly
+    // documents.forEach((doc, index) => {
+    //     if (doc.file) {
+    //         console.log(`Appending document ${index}:`, doc);
+    //         formData.append(`documents[${index}][docName]`, doc.docName);
+    //         formData.append(`documents[${index}][file]`, doc.file);
+    //     }
+    // });
+
     try {
-      setIsLoading(true);
-      const newQuotation = await addQuotationService({
-        header,
-        quotation: formData,
-      });
-      if (newQuotation.status === "success") {
-        navigate(leadsPath());
-      }
+        setIsLoading(true);
+        const newQuotation = await addQuotationService({
+            header,
+            quotation: formData,
+        });
+
+        if (newQuotation.status === "success") {
+            navigate(leadsPath());
+        }
     } catch (error: any) {
-      const err = await error;
-      toast.error(err.message);
+        const err = await error;
+        toast.error(err.message);
     } finally {
-      setIsLoading(false);
+        setIsLoading(false);
     }
-  };
+};
+
   const callEditLeadAPI = async (leadForm: any, leadId: string) => {
     try {
       const newLead = await editLeadService({
@@ -373,31 +458,40 @@ const AddQuotation = () => {
                           {"Documents"}
                         </Typography>
                         <Grid container>
-                          {documents.map((document, index) => (
-                            <Grid item md={4} key={index}>
-                              <Typography
-                                className="text-addButton"
-                                variant="subtitle1"
-                                component="h4"
-                              >
-                                {document.docName}
-                              </Typography>
-                              {document.file.endsWith(".pdf") ? (
-                                <embed
-                                  src={document.file}
-                                  type="application/pdf"
-                                  width="400"
-                                  height="400"
-                                />
-                              ) : (
-                                <img
-                                  src={document.file}
-                                  alt={document.docName}
-                                  style={{ maxWidth: "100%" }}
-                                />
-                              )}
-                            </Grid>
-                          ))}
+                          {documents.map((document, index) => {
+                            const fileURL =
+                              typeof document.file === "string"
+                                ? document.file
+                                : URL.createObjectURL(document.file);
+                            const isPDF = fileURL.endsWith(".pdf");
+
+                            return (
+                              <Grid item md={4} key={index}>
+                                <Typography
+                                  className="text-addButton"
+                                  variant="subtitle1"
+                                  component="h4"
+                                >
+                                  {document.docName}
+                                </Typography>
+
+                                {isPDF ? (
+                                  <embed
+                                    src={fileURL}
+                                    type="application/pdf"
+                                    width="400"
+                                    height="400"
+                                  />
+                                ) : (
+                                  <img
+                                    src={fileURL}
+                                    alt={document.docName}
+                                    style={{ maxWidth: "100%" }}
+                                  />
+                                )}
+                              </Grid>
+                            );
+                          })}
                         </Grid>
                       </Grid>
                       <Grid container mt={3}>
@@ -477,7 +571,7 @@ const AddQuotation = () => {
                           )}
                         </Field>
                       </Grid>
-                      <Grid item lg={4} md={4} sm={6} xs={12}>
+                      {/* <Grid item lg={4} md={4} sm={6} xs={12}>
                         <input
                           id={`file`}
                           type="file"
@@ -490,7 +584,107 @@ const AddQuotation = () => {
                           }}
                         />
                         <span style={{ color: "red" }}>{errorMessage}</span>
+                      </Grid> */}
+                      {/* new code added  */}
+                      <Grid item md={12} mt={2}>
+                        <Button
+                          variant="outlined"
+                          onClick={handleClickAddDocument}
+                        >
+                          Add More Document
+                        </Button>
+                        <Typography variant="body1" gutterBottom mr={4}>
+                          {"Image should be <= 4MB."}
+                        </Typography>
                       </Grid>
+                      <Grid item md={12}>
+                        <Grid item lg={12} md={12} sm={12} xs={12}>
+                          <span style={{ color: "red" }}>{errorMessage}</span>
+                        </Grid>
+                        {documents.map((doc, index) => (
+                          <Grid item key={index} md={12} xs={12}>
+                            <Grid container spacing={2} mt={1}>
+                              <Grid item lg={4} md={4} sm={4} xs={12}>
+                                <Autocomplete
+                                  value={
+                                    documentTypes.find(
+                                      (option) => option.value === doc.docName
+                                    ) || null
+                                  }
+                                  onChange={(e, newValue) =>
+                                    handleChangeDocumentName(newValue!, index)
+                                  }
+                                  options={documentTypes}
+                                  renderInput={(params) => (
+                                    <TextField
+                                      {...params}
+                                      className="rounded-sm w-full"
+                                      size="small"
+                                      label="Select Document"
+                                      fullWidth
+                                      variant="outlined"
+                                    />
+                                  )}
+                                />
+                                {errors[index]?.docName && (
+                                  <span>{errors[index].docName}</span>
+                                )}
+                              </Grid>
+                              <Grid item lg={4} md={4} sm={4} xs={12}>
+                                <input
+                                  id={`file ${index}`}
+                                  type="file"
+                                  onChange={(e) =>
+                                    handleFileInputChange(e, index)
+                                  }
+                                  style={{
+                                    border: "1px solid #c4c4c4",
+                                    padding: "5px",
+                                    width: "100%",
+                                    borderRadius: "5px",
+                                  }}
+                                />
+                                {errors[index]?.file && (
+                                  <span style={{ color: "red" }}>
+                                    {errors[index].file}
+                                  </span>
+                                )}
+                              </Grid>
+                              <Grid item lg={4} md={4} sm={4} xs={4}>
+                                {documents.length !== 1 && (
+                                  <Tooltip title={"Delete Image"}>
+                                    <IconButton
+                                      color="primary"
+                                      aria-label={"Delete Image"}
+                                      component="span"
+                                      onClick={() =>
+                                        handleClickDeleteDocument(index)
+                                      }
+                                    >
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="size-6"
+                                      >
+                                        <path
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                        />
+                                      </svg>
+                                    </IconButton>
+                                  </Tooltip>
+                                )}
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        ))}
+                      </Grid>
+                      {/* \end{code} */}
+
                       <Grid item lg={12} md={12} sm={12} xs={12}>
                         <Field name="comments">
                           {({ input, meta }) => (
