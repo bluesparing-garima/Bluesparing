a4c673dc04002fedaab1b7b2ea050a30aedcce78
Durgesh2008 <durgeshkm2000@gmail.com>
2025-03-26 17:40:43 +0530

    add error page

--- src/components/Admin/Team/Teams/Teams.tsx
+++ src/components/Admin/Team/Teams/Teams.tsx
@@ -1,21 +1,20 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { MaterialReactTable, type MRT_ColumnDef } from "material-react-table";
 import {
-  DAYJS_DISPLAY_FORMAT,
   DAYJS_DISPLAY_FORMAT_TABLES,
   header,
   imagePath,
   SafeKaroUser,
   TEAM_STORAGE_KEY,
 } from "../../../../context/constant";
-import { IAppUser, ITeamForm, ITeams, ITeamsVM } from "../ITeam";
+import { IAppUser, } from "../ITeam";
 import { Button, IconButton, Paper, Tooltip, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { teamEditPath, teamAddPath } from "../../../../sitemap";
 import dayjs from "dayjs";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
-import { convertITeamVMToITeamForm } from "../../../../api/Team/convertITeamVMToITeamForm";
+
 import getTeamService from "../../../../api/Team/GetTeams/getTeamsService";
 import editTeamService from "../../../../api/Team/EditTeam/editTeamService";
 import {
@@ -23,16 +22,23 @@ import {
   savePaginationState,
 } from "../../../../utils/PaginationHandler";
 import generateFormData from "../../../../utils/generateFromData";
+import OverlayError from "../../../../utils/ui/OverlayError";
+import OverlayLoader from "../../../../utils/ui/OverlayLoader";
 
 const Teams = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [teams, setTeams] = useState<IAppUser[]>([]);
+  const [errMsg, setErrMsg] = useState("");
   let storedTheme: any = localStorage.getItem("user") as SafeKaroUser | null;
   let UserData = storedTheme ? JSON.parse(storedTheme) : storedTheme;
   const [pagination, setPagination] = useState({
     pageIndex: 0,
     pageSize: 10,
   });
+
+  const onClose = () => {
+    setErrMsg("")
+  }
   useEffect(() => {
     const p = getPaginationState(TEAM_STORAGE_KEY);
     setPagination(p);
@@ -136,7 +142,7 @@ const Teams = () => {
       .then((updatedTeam) => {
         GetTeams();
       })
-      .catch((response) => {})
+      .catch((response) => { })
       .finally(() => {
         updateLoading();
       });
@@ -147,7 +153,7 @@ const Teams = () => {
       .then((updatedTeam) => {
         GetTeams();
       })
-      .catch((response) => {})
+      .catch((response) => { })
       .finally(() => {
         updateLoading();
       });
@@ -240,7 +246,7 @@ const Teams = () => {
                 Add Team
               </Button>
             </div>
-            {}
+            { }
             <hr
               className="mt-4"
               style={{ width: "100%", borderColor: "grey-800" }}
@@ -345,35 +351,18 @@ const Teams = () => {
                     </svg>
                   </IconButton>
                 </Tooltip>
-                {/* <Tooltip title={"Delete"}>
-                  <IconButton
-                    color="primary"
-                    aria-label={"Delete Team"}
-                    component="span"
-                    onClick={() =>
-                      handleClickDeleteTeam(row.original as ITeamsVM)
-                    }
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-5 text-safekaroDarkOrange"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </IconButton>
-                </Tooltip> */}
+
               </div>
             )}
           />
         </Paper>
+        {
+          errMsg && <OverlayError title="Failed" onClose={onClose} msg={errMsg} />
+        }
+        {
+          isLoading && <OverlayLoader />
+        }
+
       </div>
     </>
   );
