f377fa6e486d4b92948e8e9636961bf7b9c51c1e
bluesparing-abhishek6848 <bluesparingabhishek@gmail.com>
2025-04-04 16:08:03 +0530

    ui changes in fueltype of admin

--- src/components/Admin/FuelType/FuelTypes/FuelTypes.tsx
+++ src/components/Admin/FuelType/FuelTypes/FuelTypes.tsx
@@ -135,7 +135,7 @@ const FuelTypes = () => {
   };
   return (
     <>
-      <div className="bg-blue-200 md:p-7 p-2">
+      <div className="md:p-7 p-2">
         <Paper elevation={3} style={{ padding: 30 }}>
           <Typography className="text-safekaroDarkOrange" variant="h5">
             Fuel Type Table
@@ -165,6 +165,20 @@ const FuelTypes = () => {
             />
           </Typography>
           <MaterialReactTable
+           muiTablePaperProps={{
+            sx: {
+              boxShadow: "none", 
+              backgroundColor: "transparent", 
+            
+            },
+          }}
+    
+          muiTableContainerProps={{
+            sx: {
+              boxShadow: "none", 
+              backgroundColor: "transparent", 
+            },
+          }}
             state={{ isLoading, pagination }}
             columns={columns}
             data={parsedData}
